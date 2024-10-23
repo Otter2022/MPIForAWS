44083b404ba29989be14502126ab7431276cb468
Otter2022 <ottoz@live.com>
2024-10-22 23:24:49 -0500

    forgot to add security group functions

--- aws/security_group_manager.go
+++ aws/security_group_manager.go
@@ -4,21 +4,23 @@
 package aws
 
 import (
+	"context"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 // Create a new security group
-func CreateSecurityGroup(svc *ec2.EC2, groupName, vpcId string) (*ec2.CreateSecurityGroupOutput, error) {
+func CreateSecurityGroup(svc *ec2.Client, groupName, vpcId string) (*ec2.CreateSecurityGroupOutput, error) {
 	input := &ec2.CreateSecurityGroupInput{
 		GroupName:   aws.String(groupName),
 		Description: aws.String("Security group for gRPC MPI project"),
 		VpcId:       aws.String(vpcId),
 	}
 
-	result, err := svc.CreateSecurityGroup(input)
+	result, err := svc.CreateSecurityGroup(context.TODO(), input)
 	if err != nil {
 		log.Fatalf("Failed to create security group: %v", err)
 		return nil, err
@@ -29,11 +31,44 @@ func CreateSecurityGroup(svc *ec2.EC2, groupName, vpcId string) (*ec2.CreateSecu
 }
 
 // Add ingress rule (e.g., allow SSH)
-func AuthorizeSecurityGroupIngress(svc *ec2.EC2, groupId string) {
-	// Add logic to allow traffic on specific ports
+func AuthorizeSecurityGroupIngress(svc *ec2.Client, groupId string) {
+	input := &ec2.AuthorizeSecurityGroupIngressInput{
+		GroupId: aws.String(groupId),
+		IpPermissions: []types.IpPermission{
+			{
+				IpProtocol: aws.String("tcp"),
+				FromPort:   aws.Int32(22), // Port 22 for SSH
+				ToPort:     aws.Int32(22),
+				IpRanges: []types.IpRange{
+					{
+						CidrIp:      aws.String("0.0.0.0/0"), // Allow from all IPs
+						Description: aws.String("Allow SSH"),
+					},
+				},
+			},
+		},
+	}
+
+	// Call AuthorizeSecurityGroupIngress API
+	_, err := svc.AuthorizeSecurityGroupIngress(context.TODO(), input)
+	if err != nil {
+		log.Fatalf("Failed to authorize ingress for security group %s: %v", groupId, err)
+	}
+
+	log.Printf("Successfully added ingress rule to security group %s", groupId)
 }
 
 // Delete a security group
-func DeleteSecurityGroup(svc *ec2.EC2, groupId string) {
-	// Add logic for deletion
+func DeleteSecurityGroup(svc *ec2.Client, groupId string) {
+	input := &ec2.DeleteSecurityGroupInput{
+		GroupId: aws.String(groupId),
+	}
+
+	// Call DeleteSecurityGroup API
+	_, err := svc.DeleteSecurityGroup(context.TODO(), input)
+	if err != nil {
+		log.Fatalf("Failed to delete security group %s: %v", groupId, err)
+	}
+
+	log.Printf("Successfully deleted security group: %s", groupId)
 }
