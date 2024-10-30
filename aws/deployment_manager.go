df97947deede7f6de8505c978341e0d9a802a654
Otter2022 <ottoz@live.com>
2024-10-30 01:10:04 -0500

    updated something

--- aws/deployment_manager.go
+++ aws/deployment_manager.go
@@ -9,10 +9,18 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
-// getInstanceIPs fetches IP addresses of all instances in the specified subnet
-func getInstanceIPs(client *ec2.Client, subnetID string) ([]string, error) {
+// InstanceInfo holds the instance ID and IP address
+type InstanceInfo struct {
+	InstanceID   string
+	PrivateIP    string
+	InstanceRank int
+}
+
+// GetInstanceIPs fetches the instance IDs and IP addresses of all instances in the specified subnet
+func GetInstanceIPs(client *ec2.Client, subnetID string) ([]InstanceInfo, error) {
 	input := &ec2.DescribeInstancesInput{
 		Filters: []ec2Types.Filter{
 			{
@@ -22,20 +30,56 @@ func getInstanceIPs(client *ec2.Client, subnetID string) ([]string, error) {
 		},
 	}
 
-	var ips []string
+	var instances []InstanceInfo
 	paginator := ec2.NewDescribeInstancesPaginator(client, input)
 	for paginator.HasMorePages() {
 		page, err := paginator.NextPage(context.TODO())
 		if err != nil {
-			return nil, fmt.Errorf("failed to get page of instances: %v", err)
+			return nil, err
 		}
 		for _, reservation := range page.Reservations {
 			for _, instance := range reservation.Instances {
-				if instance.PrivateIpAddress != nil {
-					ips = append(ips, *instance.PrivateIpAddress)
+				if instance.InstanceId != nil && instance.PrivateIpAddress != nil {
+					instances = append(instances, InstanceInfo{
+						InstanceID: *instance.InstanceId,
+						PrivateIP:  *instance.PrivateIpAddress,
+					})
 				}
 			}
 		}
 	}
-	return ips, nil
+	return instances, nil
+}
+
+func InitializeEnviroments(client *ssm.Client, instances []InstanceInfo) ([]instanceInfo, error) {
+	n := len(instances)
+
+	for i, instance := range instances {
+		var commands []string
+		commands = append(commands, fmt.Sprintf("EXPORT MPI_SIZE=%d", n))
+		commands = append(commands, fmt.Sprintf("EXPORT MPI_RANK=%d", i))
+		for x := range n {
+			if x == i {
+				commands = append(commands, fmt.Sprintf("EXPORT MPI_ADDRESS_%d=\"%0.0.0.0:50051\"", i))
+				instances[i].InstanceRank = i
+			} else {
+				commands = append(commands, fmt.Sprintf("EXPORT MPI_ADDRESS_%d=\"%v\"", i, instance.PrivateIP))
+			}
+		}
+		input := &ssm.SendCommandInput{
+			DocumentName: aws.String("Make enviroment variables"),
+			Parameters: map[string][]string{
+				"commands": commands,
+			},
+			InstanceIds: []string{instances[i].InstanceID},
+		}
+		result, err := client.SendCommand(context.TODO(), input)
+		if err != nil {
+			return nil, err
+		} else {
+			fmt.Printf("%v", result)
+		}
+	}
+
+	return instances, nil
 }
--- internal/mpi/worker.go
+++ internal/mpi/worker.go
@@ -4,7 +4,24 @@
 
 package mpi
 
+import (
+	"fmt"
+	"log"
+)
+
 // StartWorker starts the worker node logic. Master nodes send messages, while other nodes receive them.
-func StartWorker(config *Config) {
-	return
+func StartWorker(nodeRank int, totalNodes int) {
+	if nodeRank == 0 {
+		// Master node logic
+		for i := 1; i < totalNodes; i++ {
+			targetIP := fmt.Sprintf("node%d.internal:50051", i) // Replace with actual IP or hostname
+			err := MPI_Send(targetIP, fmt.Sprintf("Hello from master to node %d", i), nodeRank)
+			if err != nil {
+				log.Printf("Failed to send message to node %d: %v", i, err)
+			}
+		}
+	} else {
+		// Worker node logic
+		MPI_Recv()
+	}
 }
